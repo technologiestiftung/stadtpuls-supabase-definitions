b2d225d41a7ab8fd96062fadc28d98eb5d7d0a92
Lucas Vogel <contact@vogelino.com>
2021-12-13 16:09:04 +0100

    refactor(STADTPULS-625): Rename "Unit Counter" to "Zaehler"

--- index.d.ts
+++ index.d.ts
@@ -2,12 +2,23 @@ import { definitions } from './index.d';
 import { definitions as generatedDefinitions } from './generated.d';
 export { parameters, paths, operations, external } from './generated.d'
 
-export interface definitions extends Omit<generatedDefinitions, 'records' | 'extended_user_profiles'> {
+export interface definitions extends Omit<generatedDefinitions, 'records' | 'extended_user_profiles' | 'categories'> {
   records: Omit<generatedDefinitions['records'], 'measurements'> & {
     measurements: number[];
   };
   extended_user_profiles: Omit<generatedDefinitions['extended_user_profiles'], 'categories' | 'sensors'> & {
     categories: number[];
     sensors: number[];
+  };
+  records: Omit<generatedDefinitions['categories'], 'name'> & {
+    name:
+      | "Temperatur"
+      | "CO2"
+      | "Luftfeuchtigkeit"
+      | "Luftdruck"
+      | "Zähler"
+      | "Lautstärke"
+      | "Helligkeit"
+      | "Sonstige";
   };
 }
